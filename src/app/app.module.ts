4207310890f2db278260fcc71e7df9a449f11e04
Alex Storm <alex.ellegaard@yahoo.com>
2021-03-21 16:20:29 +0100

    29 - Added fxLayoutmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -29,7 +30,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
