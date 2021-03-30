21ed5e7ec39fce59e63689912255c9e5fc04b950
Alex Storm <als@edora.hq>
2021-03-30 13:32:27 +0200

    Added functionality and design to current training

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-training',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./training.component.css']
 })
 export class TrainingComponent implements OnInit {
+  onGoingTraining = false;
 
   constructor() { }
 
